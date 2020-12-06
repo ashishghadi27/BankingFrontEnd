5430c376cb3a00f218010912bc7155d300928c48
ashishghadi27 <ashishghadi27@gmail.com>
2020-12-06 08:55:09 +0530

    User Login

---
+++ src/app/UserAuthentication/Login/user_login.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'user-login',
+    templateUrl: 'user_login.component.html',
+    styleUrls: ['./user_login.component.css']
+})
+
+export class UserLogin{
+
+}
