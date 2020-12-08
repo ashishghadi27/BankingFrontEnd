c3a12d9ac9385f4e005ee6d85207171a890bc3af
Mahima <parmar.mahima08@gmail.com>
2020-12-08 12:27:35 +0530

    all 4 html files commited and account summary ts and css commited

---
+++ src/app/UserDashboard/AccountSummary/account_summary.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'account-summary',
+    templateUrl: 'account_summary.component.html',
+    styleUrls: ['./account_summary.component.css']
+})
+
+export class AccountSummary{
+
+}
